2cdba5c2e3073fbf45feb4db42597854362e5312
CircleLiu <1176092962@qq.com>
2018-10-27 17:03:14 +0800

    Public page

--- FrontEnd/boohub/src/Routes.jsx
+++ FrontEnd/boohub/src/Routes.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Router, Route, Switch } from 'react-router'
 import createBrowserHistory from 'history/createBrowserHistory'
 import Home from './Containers/Home'
-import Publish from './Containers/Publish'
+import PublishItemWrapper from './Containers/Publish'
 
 const history = createBrowserHistory()
 
@@ -10,7 +10,7 @@ const router = App => (
     <Router history={history}>
         <Switch>
             <Route path="/" exact component={Home} />
-            <Route path="/publish" exact component={Publish} />
+            <Route path="/publish" exact component={PublishItemWrapper} />
         </Switch>
     </Router>
 )
