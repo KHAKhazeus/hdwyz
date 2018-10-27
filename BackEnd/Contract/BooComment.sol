dc68e92f72e3c54feec5024e62014cdc0613b9a9
Annie <326867996@qq.com>
2018-10-27 14:25:17 +0800

    Update BackEnd/Contract/BooComment.sol

--- BackEnd/Contract/BooComment.sol
+++ BackEnd/Contract/BooComment.sol
@@ -1,5 +1,19 @@
 pragma solidity 0.5.0.0
 
 contract BooComment {
+    struct Comment{
+        address client;
+        uint256 time;
+        string title;
+        string contians;
+        enum category{milktea,fastfood,clothes,sevenEleven,supermarket};
+        struct location{
+            enum province{SH,BJ,NYC};
+            enum ditrict{YP,CY,MHT};
+            enum street{SP,CSK,TS};
+        };
+    }
+
+    
 
 }
