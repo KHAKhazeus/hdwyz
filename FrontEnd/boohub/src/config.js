ca6775f6f9a3c3ec7869cc607d562dd00879c174
KHAKhazeus <khazeus@outlook.com>
2018-10-28 00:25:30 +0800

    More

--- FrontEnd/boohub/src/config.js
+++ FrontEnd/boohub/src/config.js
@@ -1,5 +1,6 @@
 const config = {
-    contractAddress: '0x7150761532f90aD0881c959e08737770ee8Ee15a'
+    contractAddress: '0x7150761532f90aD0881c959e08737770ee8Ee15a' +
+        ''
 };
 
 module.exports = config;
