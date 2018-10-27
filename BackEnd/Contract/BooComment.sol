0af0acecf69e431550239ba5fe23e40139458863
Annie <326867996@qq.com>
2018-10-27 15:21:11 +0800

    Update BackEnd/Contract/BooComment.sol

--- BackEnd/Contract/BooComment.sol
+++ BackEnd/Contract/BooComment.sol
@@ -2,18 +2,48 @@ pragma solidity 0.5.0.0
 
 contract BooComment {
     struct Comment{
-        address client;
-        uint256 time;
+        //address client;
+        //uint256 time;
         string title;
         string contians;
-        enum category{milktea,fastfood,clothes,sevenEleven,supermarket};
-        struct location{
-            enum province{SH,BJ,NYC};
-            enum ditrict{YP,CY,MHT};
-            enum street{SP,CSK,TS};
-        };
+        enum Category {milktea,fastfood,clothes,sevenEleven,supermarket}
+        Category public category;
+        enum Province {SH,BJ,NYC}
+        Province public province;
+        enum Ditrict {YP,CY,MHT}
+        Ditrict public ditrict;
+        enum Street {SP,CSK,TS}
+        Street public street;
     }
 
-    
+    mapping (address => mapping (uint256 => struct Comment)) private records;
+    mapping (address => uint256[]) private categories;
+
+    event Recorded(address _sender, 
+        string indexed _title,string indexed _contains,int32 indexed _category,
+        int32 indexed _province,int32 indexed _ditrict,int32 indexed _street,
+        uint256 indexed _time);
+
+    function _addToList(address from, uint256 time) private {
+        categories[from].push(time);
+    }
+
+    function getList()
+    public
+    view
+    returns (uint256[])
+    {
+        return categories[msg.sender];
+    }
+
+     function add(struct Comment texts, uint256 time) public {
+        records[msg.sender][time]=texts;
+        _addToList(msg.sender, time);
+        emit Recorded(msg.sender, texts, time);
+    }
+    function get(uint256 time) public view returns(struct Comment) {
+        
+        return records[msg.sender][time];
+    }
 
 }
